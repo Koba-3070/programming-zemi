659913576e72078a744d8a5dceaba282ab38c4f4
Koba-3070 <syuukatu2023sotu@gmail.com>
2021-10-09 06:08:49 +0000

    login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)  
     if @user.save
-      # 保存の成功をここで扱う。
+      log_in @user
       redirect_to @user
     else
       render 'new'
@@ -18,8 +18,8 @@ class UsersController < ApplicationController
   end
 
   private
-	
+
 	def user_params
-		params.require(:user).permit(:name, :email)
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
 end
